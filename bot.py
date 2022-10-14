4c9c9f5ba833b7eef13ef4ba87640b6bb11d3541
Carmensolerc <90276166+Carmensolerc@users.noreply.github.com>
2022-10-14 12:21:25 +0200

    Update bot.py
    added async cog

--- bot.py
+++ bot.py
@@ -1,20 +1,27 @@
+import asyncio
 import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv()
 bot = commands.Bot(command_prefix='$', description="AgustinJavaGod bot",intents=discord.Intents.all())
 token = os.getenv("token")
+
+load_dotenv()
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="videos explained even better than I explain"))
-    print("Ready")
+    print(f"{bot.user.name} online")
 
-from commands.ping import Ping
+async def load_extensions():
+    for file in os.listdir("./commands"):
+        if file.endswith('.py'):
+            await bot.load_extension(f'commands.{file[:-3]}')
 
-bot.add_cog(Ping(bot))
-bot.remove_command("help")
+async def  main():
+    async with bot:
+        await load_extensions()
+        await bot.start(token)
 
-bot.run(token)
+asyncio.run(main())
