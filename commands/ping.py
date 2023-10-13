c5c3cda63dfc35bef2d4470b5f4d4dc217259f32
Sonjoaquin <sonjoaquin@gmail.com>
2023-10-13 15:49:47 +0200

    Ping slash command

--- commands/ping.py
+++ commands/ping.py
@@ -13,8 +13,7 @@ class Ping(commands.Cog):
         description="Ping command"
     )
     async def ping(self, ctx):
-        latency = round(self.bot.latency * 1000)
-        await ctx.send(f'pong 🏓! {latency}ms.')
+        await ctx.send("pong 🏓!")
 
 def setup(bot):
     bot.add_cog(Ping(bot))
