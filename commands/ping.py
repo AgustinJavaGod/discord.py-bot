490061045668c47fe35ca6fe344f048a7d63b19e
Sonjoaquin <sonjoaquin@gmail.com>
2023-10-13 15:49:12 +0200

    Added ping slash command

--- commands/ping.py
+++ commands/ping.py
@@ -1,16 +1,20 @@
 from discord.ext import commands
 
 class Ping(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-	@commands.Cog.listener()
-	async def on_ready(self):
-		print("Command ping loaded")
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Command ping loaded")
 
-	@commands.command(name="ping",help="Answer pong",pass_context="True")
-	async def ping(self, ctx):
-		await ctx.send('pong 🏓')
+    @commands.slash_command(
+        name='ping',
+        description="Ping command"
+    )
+    async def ping(self, ctx):
+        latency = round(self.bot.latency * 1000)
+        await ctx.send(f'pong 🏓! {latency}ms.')
 
 def setup(bot):
-	bot.add_cog(Ping(bot))
+    bot.add_cog(Ping(bot))
