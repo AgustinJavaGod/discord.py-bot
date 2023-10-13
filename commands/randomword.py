3165ab2afcc33d3e4ee0b35e614229d64abcbae1
Sonjoaquin <sonjoaquin@gmail.com>
2023-10-13 17:20:24 +0200

    Tested randomword as slashcommand after error

--- commands/randomword.py
+++ commands/randomword.py
@@ -12,14 +12,14 @@ class Randomword(commands.Cog):
     async def on_ready(self):
         print("Command random message loaded")
 
-    @commands.command(
+    @commands.slash_command(
         name='randomword',
         description='Receive a random word with a mention'
     )
     async def randomword(self, ctx):
         user = ctx.author.mention
         randomword = random.choice(palabras)
-        await ctx.send(f'{user}, aquí tienes una palabra aleatoria: {randomword}')
+        await ctx.respond(f'{user}, aquí tienes una palabra aleatoria: {randomword}')
 
 def setup(bot):
     bot.add_cog(Randomword(bot))
